a6cc66b38e2155cd884537f70150517f5c3419f4
Julio Urrego <jut9912@gmail.com>
2024-09-01 23:35:58 -0500

    mocked auth validation and api handling

--- agriculture-api/middleware.js
+++ agriculture-api/middleware.js
@@ -1,23 +1,47 @@
 import { NextResponse } from "next/server";
 
-
+//auth function for the api
+function validateAuth(token){
+    const valid = true //mocked for now
+    console.log('validateAuth')
+    return valid
+}
 //const isLoggedIn = false;
 
 export function middleware(request){
     let headers = new Headers(request.headers)
     console.log(headers)
+
     /*if(isLoggedIn){
         return NextResponse.next()
     }*/
-   // only on case of not auth
-    //return NextResponse.redirect(new URL('/', request.url))
+
+    //if route is not api
+    /*if(!request.nextUrl.pathname.startsWith('/api/:path*')){
+        return NextResponse.next()
+    }*/
+    
+    //verify auth
+    if(!validateAuth(headers.Authorization)){
+        console.log('failed auth')
+        return NextResponse.json({
+            code: 401,
+            description: "Unauthorized"
+        },{status:401})
+    } else {
+        return NextResponse.next()
+    }
 
     //always goes next()
-    return NextResponse.next()
+    
+
+
+    // only on case of not auth
+    //return NextResponse.redirect(new URL('/', request.url))
 
 }
 
 export const config = {
-    matcher: ['/api/:path*'],
+    matcher: ['/:path*'],
 
 }
